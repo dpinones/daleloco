50cfa60a8a607ca81c1759812654b4b81fe4cf64
Damian Piñones <pinonesdamian@gmail.com>
2024-07-03 07:34:16 +0200

    add envs

--- client/dojoConfig.ts
+++ client/dojoConfig.ts
@@ -1,6 +1,11 @@
 import manifest from "../dojo-starter/manifests/dev/manifest.json";
 import { createDojoConfig } from "@dojoengine/core";
 
+const rpcUrl = import.meta.env.VITE_RPC_URL || "http://localhost:5050"
+const toriiUrl = import.meta.env.VITE_TORII_URL || "http://localhost:8080"
+const masterAddress = import.meta.env.VITE_MASTER_ADDRESS || "0x6162896d1d7ab204c7ccac6dd5f8e9e7c25ecd5ae4fcb4ad32e57786bb46e03"
+const masterPrivateKey = import.meta.env.VITE_MASTER_PRIVATE_KEY || "0x1800000000300000180000000000030000000000003006001800006600"
+
 export const dojoConfig = createDojoConfig({
-    manifest,
+    manifest, rpcUrl, toriiUrl, masterAddress, masterPrivateKey
 });
